2ee7740eb6f8f48eca23b767f2f750ca0e60a6af
amith_bny <amithbnyoo6@gmail.com>
2026-01-12 23:44:55 +0530

    simpleRAG implementaion

---
+++ ask.py
@@ -0,0 +1,58 @@
+import os
+import chromadb
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHROMA_PATH = "chroma_db"
+
+chroma_client = chromadb.PersistentClient(path=CHROMA_PATH)
+
+collection = chroma_client.get_or_create_collection(
+    name="LLM_based_testing"
+)
+
+user_query = input(
+    "What do you want to know about High-Level Test Case Generation using LLM?\n\n"
+)
+
+results = collection.query(
+    query_texts=[user_query],
+    n_results=1
+)
+
+context = " ".join(results["documents"][0])
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+system_prompt = f"""
+You are a helpful assistant. You answer questions about Automatic High-Level Test Case Generation using
+Large Language Models.
+
+RULES:
+- Answer ONLY using the provided context
+- Do NOT use outside knowledge
+- Do NOT hallucinate
+- If the answer is not in the context, say: "I don't know"
+
+--------------------
+Context:
+{context}
+"""
+
+response = client.chat.completions.create(
+    model="mistralai/Mistral-7B-Instruct-v0.2:featherless-ai",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_query}
+    ],
+    temperature=0.2,
+    max_tokens=300
+)
+
+print("\n---------------------\n")
+print(response.choices[0].message.content)
---
+++ db.py
@@ -0,0 +1,40 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
+
+DATA_PATH = r"data/pdf"
+CHROMA_PATH = r"chroma_db"
+
+chroma_client = chromadb.PersistentClient(path=CHROMA_PATH)
+collection = chroma_client.get_or_create_collection(name="LLM_based_testing")
+
+loader = PyPDFDirectoryLoader(DATA_PATH)
+
+raw_data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 300,
+    chunk_overlap = 100,
+    length_function = len,
+    is_separator_regex=False,
+)
+
+chunks = text_splitter.split_documents(raw_data)
+
+document = []
+metadata = []
+idx = []
+
+i = 0
+for x in chunks:
+    document.append(x.page_content)
+    idx.append("ID"+str(i))
+    metadata.append(x.metadata)
+
+    i += 1
+
+collection.upsert(
+    documents=document,
+    metadatas=metadata,
+    ids=idx
+)
